9589c50e6595db3682d29bcb674573f18c2dc256
Stéphane Bressani <stephane@bressani.dev>
2022-10-21 20:00:19 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
                 // Products define the executables and libraries a package produces, and make them visible to other packages.
                 .library(
                         name: "SweSvg",
-                        targets: ["cwrapper"]),
+                        targets: ["SweSvg"]),
         ],
         targets: [.target(name: "cpplib"),
                   .target(name: "svglib"),
