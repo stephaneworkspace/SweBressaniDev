75e5e1f0703a16dccede124905759f3e545715e1
Stéphane Bressani <stephane@bressani.dev>
2022-11-01 13:13:18 +0100

    changement de taille

--- Sources/SweSvg/SweSwiftUi/SweSwiftUi.swift
+++ Sources/SweSvg/SweSwiftUi/SweSwiftUi.swift
@@ -14,7 +14,7 @@ public class SweSwiftUi {
     private var size: Int
 
     public init() {
-        size = 600
+        size = 300
     }
 
     public func drawCircle(circles: [Circle]) -> Path {
