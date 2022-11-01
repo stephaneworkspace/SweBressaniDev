6a4212cbbc383e113e9e3086cf73fb0b9671df97
Stéphane Bressani <stephane@bressani.dev>
2022-11-01 13:44:40 +0100

    dark light mode

--- Sources/SweSvg/SweSwiftUi/View/DrawAstrologieView.swift
+++ Sources/SweSvg/SweSwiftUi/View/DrawAstrologieView.swift
@@ -5,19 +5,30 @@
 import Foundation
 import SwiftUI
 
-public struct DrawAstrologieView: View {
-    //@Environment(\.colorScheme) var colorScheme: ColorScheme
-    @State var colorScheme: ColorScheme
+public struct DrawAstrologieLightView: View {
     @State var swe_swift_ui: SweSwiftUi
 
-    public init(colorScheme: ColorScheme, swe_swift_ui: SweSwiftUi) {
-        self.colorScheme = colorScheme
+    public init(swe_swift_ui: SweSwiftUi) {
         self.swe_swift_ui = swe_swift_ui
     }
 
     public var body: some View {
         VStack {
-            swe_swift_ui.drawCircle(circles: swe_swift_ui.circles()).stroke(/*colorScheme == .light ? .black : .white*/ .black, lineWidth: 1.0)
+            swe_swift_ui.drawCircle(circles: swe_swift_ui.circles()).stroke(.black, lineWidth: 1.0)
+        }.frame(width: swe_swift_ui.getSizeCGFLoat(), height: swe_swift_ui.getSizeCGFLoat())
+    }
+}
+
+public struct DrawAstrologieDarkView: View {
+    @State var swe_swift_ui: SweSwiftUi
+
+    public init(swe_swift_ui: SweSwiftUi) {
+        self.swe_swift_ui = swe_swift_ui
+    }
+
+    public var body: some View {
+        VStack {
+            swe_swift_ui.drawCircle(circles: swe_swift_ui.circles()).stroke(.white, lineWidth: 1.0)
         }.frame(width: swe_swift_ui.getSizeCGFLoat(), height: swe_swift_ui.getSizeCGFLoat())
     }
 }
