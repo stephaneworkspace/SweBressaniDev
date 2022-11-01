13584f866395deb556d2bdaf57249f6480f77a90
Stéphane Bressani <stephane@bressani.dev>
2022-11-01 13:06:47 +0100

    update

--- Sources/SweSvg/SweSwiftUi/View/DrawAstrologieView.swift
+++ Sources/SweSvg/SweSwiftUi/View/DrawAstrologieView.swift
@@ -6,11 +6,11 @@ import Foundation
 import SwiftUI
 
 public struct DrawAstrologieView: View {
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    //@Environment(\.colorScheme) var colorScheme: ColorScheme
     @State var swe_swift_ui: SweSwiftUi
     public var body: some View {
         VStack {
-            swe_swift_ui.drawCircle(circles: swe_swift_ui.circles()).stroke(colorScheme == .light ? .black : .white, lineWidth: 1.0)
+            swe_swift_ui.drawCircle(circles: swe_swift_ui.circles()).stroke(/*colorScheme == .light ? .black : .white*/ .black, lineWidth: 1.0)
         }.frame(width: swe_swift_ui.getSizeCGFLoat(), height: swe_swift_ui.getSizeCGFLoat())
     }
 }
