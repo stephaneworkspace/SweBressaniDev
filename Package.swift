b817ffeeb3f32e89630d29d63bcbe6789b522484
Stéphane Bressani <stephane@bressani.dev>
2022-11-09 12:23:27 +0100

    remove tvos

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
         name: "SweBressaniDev",
         platforms: [
-            .iOS(.v15),
-            .tvOS(.v13)
+            .iOS(.v15)
         ],
         products: [
                 // Products define the executables and libraries a package produces, and make them visible to other packages.
