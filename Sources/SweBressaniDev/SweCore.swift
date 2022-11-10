38ef1e7c39659ef47a1ad93919860e7ee9bbd168
Stéphane Bressani <stephane@bressani.dev>
2022-11-10 20:58:17 +0100

    ajout numerologie core

--- Sources/SweBressaniDev/SweCore.swift
+++ Sources/SweBressaniDev/SweCore.swift
@@ -1929,3 +1929,183 @@ public class TextCore {
         }
     }
 }
+
+public class NumerologieCore {
+    public enum ECycle {
+        case Formatif, Productif, Moisson
+    }
+
+    public static func reduction(iNumber: Int) -> [Int] {
+        var aiRes = [Int]()
+        var iTemp: Int = iNumber
+        var bDone: Bool = false
+        while !bDone {
+            aiRes.append(iTemp)
+            if String(iTemp).count == 1 {
+                bDone = true
+            }
+            var iTemp2: Int = 0
+            for i in 0...String(iTemp).count - 1 {
+                let sTemp: String = String(iTemp)
+                let index = sTemp.index(sTemp.startIndex, offsetBy: i)
+                let cTemp: Character = sTemp[index]
+                iTemp2 = iTemp2 + cTemp.wholeNumberValue!
+            }
+            iTemp = iTemp2
+        }
+        return aiRes
+    }
+
+    public static func age(dBornDate: Date) -> Int {
+        let now = Date()
+        let calendar: NSCalendar! = NSCalendar(calendarIdentifier: .gregorian)
+        let calcAge = calendar.components(.year, from: dBornDate, to: now, options: [])
+        let age = calcAge.year!
+        return age
+    }
+
+    public static func cheminDeVie(dBornDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        let iTemp = iDay + iMonth + iYear
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+
+    public static func cheminDeVieInt(dBornDate: Date) -> Int {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        let iTemp = iDay + iMonth + iYear
+        let aiTempReduction = reduction(iNumber: iTemp)
+        return aiTempReduction.last!
+    }
+
+    public static func jPlusMInt(dBornDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        let iTemp = iDay + iMonth
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+
+    public static func jPlusAInt(dBornDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        let iTemp = iDay + iYear
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+
+    public static func unPlusDeuxInt(dBornDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "hh"
+        let iTemp = (iDay + iMonth) + (iDay + iYear)
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+
+    public static func mPlusAInt(dBornDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        let iTemp = iMonth + iYear
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+
+    public static func cycleAdjacents(dBornDate: Date, eCycle: NumerologieCore.ECycle) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        let iYear = Int(dateFormatter.string(from: dBornDate)) ?? 1980
+        dateFormatter.dateFormat = "MM"
+        let iMonth = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        dateFormatter.dateFormat = "dd"
+        let iDay = Int(dateFormatter.string(from: dBornDate)) ?? 1
+        var iTemp = 0;
+        switch eCycle {
+        case .Formatif:
+            iTemp = iMonth
+            break
+        case .Productif:
+            iTemp = iDay
+            break
+        case .Moisson:
+            iTemp = iYear
+            break
+        }
+        let aiTempReduction = reduction(iNumber: iTemp)
+        var sTemp: String = ""
+        for ai in aiTempReduction {
+            if sTemp == "" {
+                sTemp = String(ai)
+            } else {
+                sTemp = sTemp + " / " + String(ai)
+            }
+        }
+        return sTemp
+    }
+}
