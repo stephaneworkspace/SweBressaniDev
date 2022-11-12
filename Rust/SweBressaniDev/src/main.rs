f0a1e77af565ed19625ef3154fab17d37c3e20bd
Stéphane Bressani <stephane@bressani.dev>
2022-11-12 20:59:02 +0100

    ajout de rust pour réutiliser la libraiaire

--- Rust/SweBressaniDev/src/main.rs
+++ Rust/SweBressaniDev/src/main.rs
@@ -1,3 +1,16 @@
+//use crate::swe;
+mod swe;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
 fn main() {
     println!("Hello, world!");
+    // Get the version
+    let mut version = [0; 255];
+    let v = unsafe {
+        let p = version.as_mut_ptr();
+        swe::swe_version(p);
+        CStr::from_ptr(p)
+    };
+    println!("{:?}", CString::from(v).to_str().unwrap().to_string());
 }
