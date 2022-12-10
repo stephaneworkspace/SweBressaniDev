1b3b0ea0dc3a33e37ef9c36b3dda9e6f267e4027
Stéphane Bressani <stephane@bressani.dev>
2022-12-10 12:27:14 +0100

    experimentation

--- Sources/SweBressaniDev/View/SubView/Numerologie/VNumerologieCarreMagique.swift
+++ Sources/SweBressaniDev/View/SubView/Numerologie/VNumerologieCarreMagique.swift
@@ -72,64 +72,7 @@ public struct VNumerologieCarreMagique: View {
         Divider()
         VStack {
             HStack {
-                let prenom = concat(un: bsFirstName, deux: bsSecondName, trois: bsThirdName)
-                VStack {
-                    let cmGauche = NumerologieCore.cmGauche(nom: prenom)
-                    Text("Voyelle(s)")
-                    ForEach(0..<prenom.count, id: \.self) { idx in
-                        HStack {
-                            if cmGauche.0[idx].0 == 0 {
-                                Text("/")
-                            } else {
-                                Text(String(cmGauche.0[idx].0))
-                                if cmGauche.0[idx].0 != cmGauche.0[idx].1 {
-                                    Text(String(cmGauche.0[idx].1))
-                                }
-                            }
-                        }
-                    }
-                    HStack {
-                        Text("=")
-                    }
-                    HStack {
-                        Text(String(cmGauche.1.0))
-                        Text(String(cmGauche.1.1))
-                    }
-                }
-                VStack {
-                    let prenom = NumerologieCore.lettres(nom: prenom)
-                    //Text("Prénom")
-                    ForEach(0..<prenom.count, id: \.self) { idx in
-                        Text(prenom[idx])
-                    }
-                }
-                VStack {
-                    let cmDroite = NumerologieCore.cmDroite(nom: prenom)
-                    Text("Consonne(s)")
-                    ForEach(0..<prenom.count, id: \.self) { idx in
-                        HStack {
-                            if cmDroite.0[idx].0 == 0 {
-                                Text("/")
-                            } else {
-                                Text(String(cmDroite.0[idx].0))
-                                if cmDroite.0[idx].0 != cmDroite.0[idx].1 {
-                                    Text(String(cmDroite.0[idx].1))
-                                }
-                            }
-                        }
-                    }
-                    HStack {
-                        Text("=")
-                    }
-                    HStack {
-                        Text(String(cmDroite.1.0))
-                        Text(String(cmDroite.1.1))
-                    }
-                }
-            }
-            Divider()
-            HStack {
-                let nom = concat(un: bsLastName1, deux: bsLastName2, trois: bsLastName3)
+                let nom = concat(un: bsFirstName, deux: bsSecondName, trois: bsThirdName)
                 VStack {
                     let cmGauche = NumerologieCore.cmGauche(nom: nom)
                     let cmDroite = NumerologieCore.cmDroite(nom: nom)
@@ -138,8 +81,7 @@ public struct VNumerologieCarreMagique: View {
                         Spacer()
                         Text("Voyelle(s)")
                                 .multilineTextAlignment(.leading)
-                        Text("Nom")
-                                .multilineTextAlignment(.center)
+                        Spacer()
                         Text("Consonne(s)")
                                 .multilineTextAlignment(.trailing)
                         Spacer()
@@ -162,7 +104,6 @@ public struct VNumerologieCarreMagique: View {
                         Text("=")
                     }
                     HStack {
-                        Spacer()
                         if cmGauche.1.2 {
                             Text(String(cmGauche.1.0) + "*")
                                     .multilineTextAlignment(.center)
@@ -192,25 +133,37 @@ public struct VNumerologieCarreMagique: View {
                             Text(String(cmDroite.1.1))
                                     .multilineTextAlignment(.center)
                         }
-                        Spacer()
                     }
                 }
             }
+            Divider()
             HStack {
                 let nom = concat(un: bsLastName1, deux: bsLastName2, trois: bsLastName3)
                 VStack {
                     let cmGauche = NumerologieCore.cmGauche(nom: nom)
-                    Text("Voyelle(s)")
+                    let cmDroite = NumerologieCore.cmDroite(nom: nom)
+                    let nomL = NumerologieCore.lettres(nom: nom)
+                    HStack {
+                        Spacer()
+                        Text("Voyelle(s)")
+                                .multilineTextAlignment(.leading)
+                        Spacer()
+                        Text("Consonne(s)")
+                                .multilineTextAlignment(.trailing)
+                        Spacer()
+                    }
                     ForEach(0..<nom.count, id: \.self) { idx in
                         HStack {
-                            if cmGauche.0[idx].0 == 0 {
-                                Text("/")
-                            } else {
-                                Text(String(cmGauche.0[idx].0))
-                                if cmGauche.0[idx].0 != cmGauche.0[idx].1 {
-                                    Text(String(cmGauche.0[idx].1))
-                                }
-                            }
+                            Spacer()
+                            Text(String(cmGauche.0[idx].1))
+                                    .multilineTextAlignment(.leading)
+                            Spacer()
+                            Text(nomL[idx])
+                                    .multilineTextAlignment(.center)
+                            Spacer()
+                            Text(String(cmDroite.0[idx].1))
+                                    .multilineTextAlignment(.trailing)
+                            Spacer()
                         }
                     }
                     HStack {
@@ -219,51 +172,32 @@ public struct VNumerologieCarreMagique: View {
                     HStack {
                         if cmGauche.1.2 {
                             Text(String(cmGauche.1.0) + "*")
+                                    .multilineTextAlignment(.center)
                         } else {
                             Text(String(cmGauche.1.0))
+                                    .multilineTextAlignment(.center)
                         }
                         if cmGauche.1.3 {
                             Text(String(cmGauche.1.1) + "*")
+                                    .multilineTextAlignment(.center)
                         } else {
                             Text(String(cmGauche.1.1))
+                                    .multilineTextAlignment(.center)
                         }
-                    }
-                }
-                VStack {
-                    let nom = NumerologieCore.lettres(nom: nom)
-                    //Text("Nom")
-                    ForEach(0..<nom.count, id: \.self) { idx in
-                        Text(nom[idx])
-                    }
-                }
-                VStack {
-                    let cmDroite = NumerologieCore.cmDroite(nom: nom)
-                    Text("Consonne(s)")
-                    ForEach(0..<nom.count, id: \.self) { idx in
-                        HStack {
-                            if cmDroite.0[idx].0 == 0 {
-                                Text("/")
-                            } else {
-                                Text(String(cmDroite.0[idx].0))
-                                if cmDroite.0[idx].0 != cmDroite.0[idx].1 {
-                                    Text(String(cmDroite.0[idx].1))
-                                }
-                            }
-                        }
-                    }
-                    HStack {
-                        Text("=")
-                    }
-                    HStack {
+                        Spacer()
                         if cmDroite.1.2 {
                             Text(String(cmDroite.1.0) + "*")
+                                    .multilineTextAlignment(.center)
                         } else {
                             Text(String(cmDroite.1.0))
+                                    .multilineTextAlignment(.center)
                         }
                         if cmDroite.1.3 {
                             Text(String(cmDroite.1.1) + "*")
+                                    .multilineTextAlignment(.center)
                         } else {
                             Text(String(cmDroite.1.1))
+                                    .multilineTextAlignment(.center)
                         }
                     }
                 }
