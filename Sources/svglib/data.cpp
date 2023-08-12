b4a78e75ca351f5674b91ee27a1bf6cac6276631
Stéphane Bressani <stephane@bressani.dev>
2023-08-13 01:51:57 +0200

    amélioration

--- Sources/svglib/data.cpp
+++ Sources/svglib/data.cpp
@@ -105,16 +105,34 @@ void Data::close_by() {
 
 string Data::generate() {
     std::ostringstream oss;
-
+    /*
+    string fill;
+    fill.assign(properties.fill.fill_cchar);
+    string stroke;
+    stroke.assign(properties.stroke.stroke_cchar);
+    */
+    string d;
+    int l = 0;
     for (int i = 0; i < idx_data; ++i) {
-        oss << vec_data[i].data;
+        if (l > 0) {
+            d += " ";
+        }
+        d += vec_data[i].data;
         int k = 0;
         for (int j = 0; j < idx_point; ++j) {
             if (vec_point[j].point_idx == vec_data[i].point_idx) {
-                oss << (k++ ? "," : "") << vec_point[j].point;
+                if (k > 0) {
+                    d += ",";
+                }
+                if ((int) vec_point[j].point == vec_point[j].point ) {
+                    d += to_string((int) vec_point[j].point);
+                } else {
+                    d += Data::round(vec_point[j].point);
+                }
+                k++;
             }
         }
-        if (i < idx_data - 1) oss << " ";
+        l++;
     }
 
     oss << "<path d=\"" << oss.str()
