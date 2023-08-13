3b670d3329a2fa8608f0f65850082f8d9f1d8e87
Stéphane Bressani <stephane@bressani.dev>
2023-08-13 02:02:15 +0200

    amélioration

--- Sources/svglib/document.cpp
+++ Sources/svglib/document.cpp
@@ -6,13 +6,11 @@
 
 using namespace svglib;
 
-Document::Document(double w, double h) {
-    width = w;
-    height = h;
-    content = "";
-}
+Document::Document(double w, double h) : width(w), height(h), content("") {}
 
 string Document::generate() {
-    string s = "<svg viewBox=\"0 0 " + to_string((int) width) + " " + to_string((int) height) + "\" xmlns=\"http://www.w3.org/2000/svg\">" + content + "</svg>";
-    return s;
+    std::ostringstream oss;
+    oss << "<svg viewBox=\"0 0 " << static_cast<int>(width) << " " << static_cast<int>(height)
+        << "\" xmlns=\"http://www.w3.org/2000/svg\">" << content << "</svg>";
+    return oss.str();
 }
